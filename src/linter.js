bd92705b90cf5661731543c7314bf9e2b3352d14
Anentropic <ego@anentropic.com>
2019-07-23 22:36:22 +0100

    use a node version supported by brew nodenv, indent with spaces

--- src/linter.js
+++ src/linter.js
@@ -37,22 +37,22 @@ const SEPARATOR = "";
  *
  * @param {string} ignoreFile - Content of an .apiblint ignore file
  * @returns {Map<string, Array<Object>>} Parsed representation of ignore file
- *		an map of arrays of position objects keyed by prefixed warning code
+ *    an map of arrays of position objects keyed by prefixed warning code
  */
 export function parseIgnoreFile(ignoreFile) {
-	let ignores = new Map();
-	if (ignoreFile) {
-		let lines = ignoreFile.split(NEWLINE).filter(line => Boolean(line));
-		lines.forEach(line => {
-			let [code, startLine, endLine] = line.split(":");
-			ignores.has(code) || ignores.set(code, []);
-			ignores.get(code).push({
-				startLine: parseInt(startLine),
-				endLine: parseInt(endLine),
-			});
-		});
-	};
-	return ignores;
+  let ignores = new Map();
+  if (ignoreFile) {
+    let lines = ignoreFile.split(NEWLINE).filter(line => Boolean(line));
+    lines.forEach(line => {
+      let [code, startLine, endLine] = line.split(":");
+      ignores.has(code) || ignores.set(code, []);
+      ignores.get(code).push({
+        startLine: parseInt(startLine),
+        endLine: parseInt(endLine),
+      });
+    });
+  };
+  return ignores;
 }
 
 /**
@@ -62,45 +62,45 @@ export function parseIgnoreFile(ignoreFile) {
  *
  * @param {Array<string>} lines - The .apib source file as array of lines
  * @param {number} contextSize - How many lines ± of context to display around
- *		the lines highlighted by the warning code-span
+ *    the lines highlighted by the warning code-span
  * @param {Object} warning - Parsed warning object from `parseWarning`
  * @returns {string} The formatted warning details
  */
 export function formatWarning(lines, contextSize, warning) {
-	let formatted = [];
-	let formatLineNo = partialRight(lpad, String(lines.length).length, {suffix: LINE_NO_SEP});
-	for (let i of range(
-		Math.max(0, warning.startLine - contextSize),
-		Math.min(lines.length, warning.endLine + contextSize + 1)
-	)) {
-		let line = lines[i];
-		if (i == warning.startLine) {
-			// first highlighted line
-			let pre = line.slice(0, warning.startChar);
-			let post = line.slice(warning.startChar);
-			formatted.push(
-				chalk.yellow(formatLineNo(i)) + chalk.gray(pre) + post
-			);
-		} else if (i > warning.startLine && i < warning.endLine) {
-			// fully highlighted line
-			formatted.push(
-				chalk.yellow(formatLineNo(i)) + line
-			);
-		} else if (i == warning.endLine) {
-			// last highlighted line
-			let pre = line.slice(0, warning.endChar);
-			let post = line.slice(warning.endChar);
-			formatted.push(
-				chalk.yellow(formatLineNo(i)) + pre + chalk.gray(post)
-			);
-		} else {
-			// pre/post 'context' lines
-			formatted.push(
-				chalk.yellow.dim(formatLineNo(i)) + chalk.gray(line)
-			);
-		}
-	};
-	return formatted;
+  let formatted = [];
+  let formatLineNo = partialRight(lpad, String(lines.length).length, {suffix: LINE_NO_SEP});
+  for (let i of range(
+    Math.max(0, warning.startLine - contextSize),
+    Math.min(lines.length, warning.endLine + contextSize + 1)
+  )) {
+    let line = lines[i];
+    if (i == warning.startLine) {
+      // first highlighted line
+      let pre = line.slice(0, warning.startChar);
+      let post = line.slice(warning.startChar);
+      formatted.push(
+        chalk.yellow(formatLineNo(i)) + chalk.gray(pre) + post
+      );
+    } else if (i > warning.startLine && i < warning.endLine) {
+      // fully highlighted line
+      formatted.push(
+        chalk.yellow(formatLineNo(i)) + line
+      );
+    } else if (i == warning.endLine) {
+      // last highlighted line
+      let pre = line.slice(0, warning.endChar);
+      let post = line.slice(warning.endChar);
+      formatted.push(
+        chalk.yellow(formatLineNo(i)) + pre + chalk.gray(post)
+      );
+    } else {
+      // pre/post 'context' lines
+      formatted.push(
+        chalk.yellow.dim(formatLineNo(i)) + chalk.gray(line)
+      );
+    }
+  };
+  return formatted;
 }
 
 /**
@@ -110,19 +110,19 @@ export function formatWarning(lines, contextSize, warning) {
  *
  * @param {Array<string>} lines - The .apib source file as array of lines
  * @param {number} contextSize - How many lines ± of context to display around
- *		the lines highlighted by the warning code-span
+ *    the lines highlighted by the warning code-span
  * @param {Object} warning - Parsed warning object from `parseWarning`
  * @returns {string} The formatted warning details
  */
 export function formatWarningHeader(warning) {
-	return [
-		chalk.red(warning.description),
-		chalk.blueBright(
-			warning.code + WARNING_CODE_SEP +
-			warning.startLine + WARNING_CODE_SEP +
-			warning.endLine
-		)
-	];
+  return [
+    chalk.red(warning.description),
+    chalk.blueBright(
+      warning.code + WARNING_CODE_SEP +
+      warning.startLine + WARNING_CODE_SEP +
+      warning.endLine
+    )
+  ];
 }
 
 /**
@@ -130,29 +130,29 @@ export function formatWarningHeader(warning) {
  * warnings, either presenting or ignoring them according to .apiblint file.
  *
  * @param {Map<string, Array<Object>>} ignores - Parsed representation of the
- *		.apiblint ignore file
+ *    .apiblint ignore file
  * @param {number} fuzzFactor - How many lines ± to fuzzy match ignore blocks
  * @param {Object} warning - Parsed warning object from `parseWarning`
  * @returns {boolean} whether to ignore this warning
  */
 export function shouldIgnore(ignores, fuzzFactor, warning) {
-	let ignorePositions = ignores.get(warning.code);
-	if (!ignorePositions) {
-		return false;
-	}
-	return ignorePositions.some(pos => {
-		return (
-			range(
-				pos.startLine - fuzzFactor,
-				pos.startLine + fuzzFactor + 1
-			).includes(warning.startLine)
-			&&
-			range(
-				pos.endLine - fuzzFactor,
-				pos.endLine + fuzzFactor + 1
-			).includes(warning.endLine)
-		);
-	});
+  let ignorePositions = ignores.get(warning.code);
+  if (!ignorePositions) {
+    return false;
+  }
+  return ignorePositions.some(pos => {
+    return (
+      range(
+        pos.startLine - fuzzFactor,
+        pos.startLine + fuzzFactor + 1
+      ).includes(warning.startLine)
+      &&
+      range(
+        pos.endLine - fuzzFactor,
+        pos.endLine + fuzzFactor + 1
+      ).includes(warning.endLine)
+    );
+  });
 }
 
 /**
@@ -160,24 +160,24 @@ export function shouldIgnore(ignores, fuzzFactor, warning) {
  *
  * @param {Object} warning - A result from drafter.validate tool
  * @returns {Object} Start and end line/char positions describing
- *		the code-span highlighted by the warning
+ *    the code-span highlighted by the warning
  */
 export function parseWarning(rawWarning) {
-		let sourcePositions = rawWarning.attributes.sourceMap.content[0].content;
-		let first = sourcePositions[0];
-		let last = sourcePositions.slice(-1)[0];
-		// sourcePosition objects contain a pair of values, so content[0]
-		// is the start info and content[1] is the end info of a pos.
-		// For some reason each sourcePosition selects a single line only
-		// so there are multiple sourcePosition objects per warning 🤷‍♂️
-		return {
-			code: WARNING_CODE_PREFIX + rawWarning.attributes.code.content,
-			description: rawWarning.content,
-			startLine: first.content[0].attributes.line.content - 1,
-			startChar: first.content[0].attributes.column.content - 1,
-			endLine: last.content[1].attributes.line.content - 1,
-			endChar: last.content[1].attributes.column.content - 1,
-		}
+    let sourcePositions = rawWarning.attributes.sourceMap.content[0].content;
+    let first = sourcePositions[0];
+    let last = sourcePositions.slice(-1)[0];
+    // sourcePosition objects contain a pair of values, so content[0]
+    // is the start info and content[1] is the end info of a pos.
+    // For some reason each sourcePosition selects a single line only
+    // so there are multiple sourcePosition objects per warning 🤷‍♂️
+    return {
+      code: WARNING_CODE_PREFIX + rawWarning.attributes.code.content,
+      description: rawWarning.content,
+      startLine: first.content[0].attributes.line.content - 1,
+      startChar: first.content[0].attributes.column.content - 1,
+      endLine: last.content[1].attributes.line.content - 1,
+      endChar: last.content[1].attributes.column.content - 1,
+    }
 }
 
 /**
@@ -191,45 +191,45 @@ export function parseWarning(rawWarning) {
  * @returns {number} An 'exitCode' value representing success or failure
  */
 export async function processWarnings(options, lines, rawWarnings, filename) {
-	log.info(chalk.red.bold(rawWarnings.length + " linting issues found"));
+  log.info(chalk.red.bold(rawWarnings.length + " linting issues found"));
 
-	let ignoreFileName = filename + options.ignoreFileExt;
-	log.info(
-		'To ignore any of these instances, copy and paste the blue\n' +
-		'<code>:<startLine>:<endLine> error position specifier\n' +
-		'into an ignore file at: ' + chalk.bold(ignoreFileName)
-	)
-	let ignoreFile = await fs.readFile(ignoreFileName, 'utf8').catch(err => {});
-	let ignores = parseIgnoreFile(ignoreFile);
+  let ignoreFileName = filename + options.ignoreFileExt;
+  log.info(
+    'To ignore any of these instances, copy and paste the blue\n' +
+    '<code>:<startLine>:<endLine> error position specifier\n' +
+    'into an ignore file at: ' + chalk.bold(ignoreFileName)
+  )
+  let ignoreFile = await fs.readFile(ignoreFileName, 'utf8').catch(err => {});
+  let ignores = parseIgnoreFile(ignoreFile);
 
-	let formatWarning_ = partial(formatWarning, lines, options.contextSize);
-	let shouldIgnore_ = partial(shouldIgnore, ignores, options.fuzzFactor);
+  let formatWarning_ = partial(formatWarning, lines, options.contextSize);
+  let shouldIgnore_ = partial(shouldIgnore, ignores, options.fuzzFactor);
 
-	let exitCode = 0;
-	rawWarnings.forEach(rawWarning => {
-		log.info(SEPARATOR);
+  let exitCode = 0;
+  rawWarnings.forEach(rawWarning => {
+    log.info(SEPARATOR);
 
-		// use module.exports so it can be mocked in tests :facepalm:
-		let warning = module.exports.parseWarning(rawWarning);
+    // use module.exports so it can be mocked in tests :facepalm:
+    let warning = module.exports.parseWarning(rawWarning);
 
-		let headers = formatWarningHeader(warning);
-		headers.forEach(line => {
-			log.info(line);
-		});
+    let headers = formatWarningHeader(warning);
+    headers.forEach(line => {
+      log.info(line);
+    });
 
-		if (shouldIgnore_(warning)) {
-			log.info(chalk.yellow('ignored by .apiblint file'));
-			return; // continue forEach
-		}
-		// not ignored
-		exitCode = 1;
+    if (shouldIgnore_(warning)) {
+      log.info(chalk.yellow('ignored by .apiblint file'));
+      return; // continue forEach
+    }
+    // not ignored
+    exitCode = 1;
 
-		formatWarning_(warning).forEach(line => {
-			log.info(line);
-		});
-	})
-	log.info(SEPARATOR);
-	return exitCode;
+    formatWarning_(warning).forEach(line => {
+      log.info(line);
+    });
+  })
+  log.info(SEPARATOR);
+  return exitCode;
 }
 
 /**
@@ -240,33 +240,33 @@ export async function processWarnings(options, lines, rawWarnings, filename) {
  * @returns {number} An 'exitCode' value representing success or failure
  */
 export async function lintFile(options, filename) {
-	log.info(chalk.bold(filename));
+  log.info(chalk.bold(filename));
 
-	let blueprint = await fs.readFile(filename, 'utf8');
-	let warnings = await validateBlueprint(blueprint, options.drafterOpts).catch(err => {
-		// 🤔 I'm not sure how to cause an 'error'
-		// (passing a non-apib file still gives a parseResult)
-		log.info(chalk.red.bold("Error:"), err);
-		return 2;
+  let blueprint = await fs.readFile(filename, 'utf8');
+  let warnings = await validateBlueprint(blueprint, options.drafterOpts).catch(err => {
+    // 🤔 I'm not sure how to cause an 'error'
+    // (passing a non-apib file still gives a parseResult)
+    log.info(chalk.red.bold("Error:"), err);
+    return 2;
   });
 
   let exitCode = 0;
   if (warnings) {
-		// ⚠️
-		if (warnings.element == 'parseResult') {
-			// typical linting results
-		  let lines = blueprint.split(NEWLINE);
-			exitCode = await processWarnings(options, lines, warnings.content, filename);
-		} else {
-			// "should not happen"
-  		log.info(chalk.red.bold("Error: unrecognised linter result"), warnings);
-  		exitCode = 3;
-		}
-	} else {
-		// 🎉
-		log.info(chalk.green('OK'));
-	};
-	return exitCode;
+    // ⚠️
+    if (warnings.element == 'parseResult') {
+      // typical linting results
+      let lines = blueprint.split(NEWLINE);
+      exitCode = await processWarnings(options, lines, warnings.content, filename);
+    } else {
+      // "should not happen"
+      log.info(chalk.red.bold("Error: unrecognised linter result"), warnings);
+      exitCode = 3;
+    }
+  } else {
+    // 🎉
+    log.info(chalk.green('OK'));
+  };
+  return exitCode;
 }
 
 /**
@@ -278,6 +278,6 @@ export async function lintFile(options, filename) {
  * @returns {Array<number>} An 'exitCode' value for each file linted
  */
 export async function lint(files, options) {
-	let lintFile_ = partial(lintFile, options)
-	return await Promise.all(files.map(lintFile_));
+  let lintFile_ = partial(lintFile, options)
+  return await Promise.all(files.map(lintFile_));
 }
--- test/test_utils.js
+++ test/test_utils.js
@@ -6,41 +6,41 @@ import { lpad } from '../src/utils';
 describe('utils', function() {
   describe('lpad', function() {
 
-	  [
-	    {args: ["20", 5], expected: "   20"},
-	    {args: ["20", 3], expected: " 20"},
-	    {args: ["20", 1], expected: "20"},
-	  ].forEach(params => {
-	    it(`should left-pad str, with whitespace and no suffix by default | ${JSON.stringify(params.args)}`, function() {
-	    	const [val, length] = params.args;
-	    	const result = lpad(val, length);
-	      assert.equal(result, params.expected);
-	    });
-  	});
+    [
+      {args: ["20", 5], expected: "   20"},
+      {args: ["20", 3], expected: " 20"},
+      {args: ["20", 1], expected: "20"},
+    ].forEach(params => {
+      it(`should left-pad str, with whitespace and no suffix by default | ${JSON.stringify(params.args)}`, function() {
+        const [val, length] = params.args;
+        const result = lpad(val, length);
+        assert.equal(result, params.expected);
+      });
+    });
 
-	  [
-	    {args: ["20", 5, {suffix: ": "}], expected: "   20: "},
-	    {args: ["20", 3, {suffix: ": "}], expected: " 20: "},
-	    {args: ["20", 1, {suffix: ": "}], expected: "20: "},
-	  ].forEach(params => {
-	    it(`should append suffix if given | ${JSON.stringify(params.args)}`, function() {
-	    	const [val, length, opts] = params.args;
-	    	const result = lpad(val, length, opts);
-	      assert.equal(result, params.expected);
-	    });
-  	});
+    [
+      {args: ["20", 5, {suffix: ": "}], expected: "   20: "},
+      {args: ["20", 3, {suffix: ": "}], expected: " 20: "},
+      {args: ["20", 1, {suffix: ": "}], expected: "20: "},
+    ].forEach(params => {
+      it(`should append suffix if given | ${JSON.stringify(params.args)}`, function() {
+        const [val, length, opts] = params.args;
+        const result = lpad(val, length, opts);
+        assert.equal(result, params.expected);
+      });
+    });
 
-	  [
-	    {args: ["20", 5, {padWith: "."}], expected: "...20"},
-	    {args: ["20", 3, {padWith: "*"}], expected: "*20"},
-	    {args: ["20", 1, {padWith: "_"}], expected: "20"},
-	  ].forEach(params => {
-	    it(`should pad with padWidth if given | ${JSON.stringify(params.args)}`, function() {
-	    	const [val, length, opts] = params.args;
-	    	const result = lpad(val, length, opts);
-	      assert.equal(result, params.expected);
-	    });
-  	});
+    [
+      {args: ["20", 5, {padWith: "."}], expected: "...20"},
+      {args: ["20", 3, {padWith: "*"}], expected: "*20"},
+      {args: ["20", 1, {padWith: "_"}], expected: "20"},
+    ].forEach(params => {
+      it(`should pad with padWidth if given | ${JSON.stringify(params.args)}`, function() {
+        const [val, length, opts] = params.args;
+        const result = lpad(val, length, opts);
+        assert.equal(result, params.expected);
+      });
+    });
 
-	});
+  });
 });
